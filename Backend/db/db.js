3f0d4a2fd40c50cc3e5b5d1b4a01fac22dd1a2b6
Amogh Kakkanavar <amogh781@gmail.com>
2026-03-16 19:19:23 +0530

    connect railway postgres

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,11 +1,10 @@
-const { Pool } = require("pg")
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "db",
-  database: "postgres",
-  password: "Iloveawp781",
-  port: 5432
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-module.exports = pool
+module.exports = pool;
